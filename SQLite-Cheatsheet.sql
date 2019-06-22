22eab62623c256097772d3d44d4b0a47123a7488
dholzheimer <52114789+dholzheimer@users.noreply.github.com>
2019-06-23 11:15:24 +1200

    Adds base file of existing work

---
+++ SQLite-Cheatsheet.sql
@@ -0,0 +1,89 @@
+--This creates a Table and defines the rows
+CREATE TABLE Author
+(authorNo INTEGER PRIMARY KEY NOT NULL,
+authorFirstName TEXT,
+authorLastName TEXT, 
+authorStreet TEXT, 
+authorSuburb TEXT); 
+
+-------------------------------
+--This Creates a Temp Table using
+-- a SELECT statement (Point Query)
+CREATE TABLE AuthorTemp 
+	AS SELECT * 
+		FROM Author
+		WHERE authorSuburb = 'Meadowbank';
+
+--------------------------------------
+--This Alters an existing Table's
+--Table Name, Column Name, Adds a column
+ALTER TABLE Author
+RENAME TO MeadowbankAuthor;
+
+ALTER TABLE Author
+RENAME COLUMN authorStreet
+	TO streetAddress;
+
+ALTER TABLE Author
+ADD authorCity TEXT;
+
+---------------------------------------
+--This Drops a table from the DB
+DROP TABLE MeadowbankAuthor;
+
+--------------------------------------
+--Inserts new values INTO specific
+--columns of a table
+INSERT INTO Author
+	(authorFirstName, authorLastName
+		authorStreet, authorSuburb,
+		authorCity)
+	 VALUES ('De Silva', 'Clarice',
+	 	'21 Park View Street', 'Park View',
+	 	'Auckland');
+
+---------------------------------------
+--Inserts table values from an existing 
+--table using a SELECT statement
+INSERT INTO AuthorTemp
+	SELECT authorFirstName, authorLastName,
+		authorStreet, authorSuburb, authorCity
+	FROM Author; 
+
+----------------------------------------
+--Selects all columns
+SELECT *     -- <---
+FROM Book;
+
+------------------------------------------
+--Selects specific columns
+SELECT bookCode 'Book Code', bookTitle 'Title'
+FROM Book;
+
+-----------------------------------------
+--Selects only unique values and removes
+--any duplicates from the results
+SELECT DISTINCT staffCity 'City' --  <---
+FROM Staff;
+
+------------------------------------------
+--Limits the number of rows that are 
+--returned by the query to 3
+SELECT *
+FROM Book
+LIMIT 3; -- 	<---
+
+-------------------------------------------
+--Limits the number of rows returned to 3 rows
+--and Offsets the rows by 2 rows
+SELECT *
+FROM Book
+LIMIT 3
+OFFSET 2;			-- 	<---
+
+-----------------------------------------
+
+
+
+
+
